c993873ab47288bdbda40aab3a1ad40ef1e91b16
bchaps1999 <chapuis@college.harvard.edul>
2020-04-23 13:26:52 -0400

    Added Shiny app

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -1,5 +1,6 @@
 
 library(shiny)
+library(shinythemes)
 library(tidyverse)
 
 dem_primary <- read_csv("dem_primary.csv") %>% 
@@ -39,7 +40,8 @@ state_list <- c(
 )
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("Predictive Capacity of Polls and Prediction Markets",
+ui <- navbarPage(theme = shinytheme("flatly"),
+    "Predictive Capacity of Polls and Prediction Markets",
     tabPanel("State Data",
         sidebarLayout(
             sidebarPanel(
@@ -52,15 +54,32 @@ ui <- navbarPage("Predictive Capacity of Polls and Prediction Markets",
             )
         )
     ),
+    tabPanel("Poll and Market Data",
+             sidebarLayout(
+                 sidebarPanel(
+                     selectInput("plotInput", "Data Source",
+                                 choices = c("Polling Average", "Prediction Market"))
+                 ),
+                 mainPanel(
+                     plotOutput("poll_market_plot")
+                 )
+             )
+    ),
+    tabPanel("Prediction Models",
+             mainPanel("main")
+             ),
     tabPanel("About",
-        mainPanel("In recent years, prediction markets like PredictIt.org have become popular ways of betting on the outcomes of political events. These markets allow you to buy a contract related to a specific possible outcome, and if that outcome actually happens, you will get a dollar. For example, you could buy a contract for __ cents that you think Bernie Sanders will win the democratic primary, and if he actually does, the website will pay you a dollar. As more people buy contracts for a specific outcome, the higher the price gets as it is seen as a more likely possibility.  
+             mainPanel(
+                 p("In recent years, prediction markets like PredictIt.org have become popular ways of betting on the outcomes of political events. These markets allow you to buy a contract related to a specific possible outcome, and if that outcome actually happens, you will get a dollar. For example, you could buy a contract for __ cents that you think Bernie Sanders will win the democratic primary, and if he actually does, the website will pay you a dollar. As more people buy contracts for a specific outcome, the higher the price gets as it is seen as a more likely possibility.  
 
-\n Some studies have suggested that these markets, which often have hundreds of thousands of contracts sold per day, may be better predictors of political outcomes than actual polls. The purpose of this project is to examine some of the relationships between data from polls and prediction markets within the context of the 2020 Democratic primary elections. The relationships that will be explored in this project include the correlation between polling results and market prices and the correlation between market prices right before a vote and the results of the vote.
+Some studies have suggested that these markets, which often have hundreds of thousands of contracts sold per day, may be better predictors of political outcomes than actual polls. The purpose of this project is to examine some of the relationships between data from polls and prediction markets within the context of the 2020 Democratic primary elections. The relationships that will be explored in this project include the correlation between polling results and market prices and the correlation between market prices right before a vote and the results of the vote.
 
-\n Polling data will come from fivethirtyeight's estimated polling average for each candidate, and will be updated regularly as more polls are released. Market data will come from PredictIt.org. Since market data for each primary state can only be accessed after the results are confirmed, more states will be added over time.")
+Polling data will come from fivethirtyeight's estimated polling average for each candidate, and will be updated regularly as more polls are released. Market data will come from PredictIt.org. Since market data for each primary state can only be accessed after the results are confirmed, more states will be added over time."
+                 )
+             )
     )
 )
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
 
     output$statePlot <- renderPlot({
@@ -83,6 +102,39 @@ server <- function(input, output) {
                                  labels = c("Prediction Market Price",
                                             "Polling Average"))
     })
+    
+    output$poll_market_plot <- renderPlot({
+        if (input$plotInput == "Prediction Market") {
+            final_results_data %>%
+                mutate(winner = ifelse(winner == 1, "Yes", "No")) %>% 
+                ggplot(aes(x = market_close, y = vote_percent)) +
+                geom_point(aes(color = winner), alpha = 0.75) +
+                stat_smooth(method="lm",
+                            formula=y ~ poly(x, 3, raw=TRUE)) +
+                theme_minimal() +
+                theme(legend.position="bottom") +
+                labs(title = "Relationship Between Final Prediction Market Price and Vote Percent",
+                     subtitle = "for Candidates in 2020 Democratic Primaries and Caucuses",
+                     x = "Final Closing Market Price on PredictIt",
+                     y = "Percent of Vote in Election",
+                     color = "Election Winner")
+        } else {
+            final_results_data %>%
+                mutate(winner = ifelse(winner == 1, "Yes", "No")) %>% 
+                ggplot(aes(x = poll_estimate, y = vote_percent)) +
+                geom_point(aes(color = winner), alpha = 0.75) +
+                stat_smooth(method="lm", formula = y~x, se=TRUE) +
+                theme_minimal() +
+                theme(legend.position="bottom") +
+                labs(title = "Relationship Between Final Polling Average and Percent of Vote",
+                     subtitle = "for Candidates in 2020 Democratic Primaries and Caucuses",
+                     x = "Final Polling Average from FiveThirtyEight",
+                     y = "Percent of Vote in Election",
+                     color = "Election Winner")
+        }
+        
+    })
+    
 }
 
 # Run the application 
