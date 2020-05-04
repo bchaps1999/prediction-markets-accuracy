a3432dcea1d67acfd7b2f35f2f6acbbdad69e885
bchaps1999 <chapuis@college.harvard.edul>
2020-05-03 23:42:24 -0400

    Updated logistic regression tab and about page, added error message to state data

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -4,11 +4,11 @@ library(shiny)
 library(shinythemes)
 library(gt)
 library(ggpubr)
+library(plotrix)
+library(tidymodels)
 library(tidyverse)
 
 dem_primary <- read_csv("dem_primary.csv")
-
-winner_predictions <- read_csv("winner_predictions.csv")
 
 final_results <- dem_primary %>%
     filter(date == contest_date - 1)
@@ -28,8 +28,12 @@ predictions <- final_results %>%
         poll_correct = ifelse(poll_winner == winner, 1, 0)
     ) %>%
     ungroup() %>%
-    filter(winner == 1) %>%
+    filter(winner == 1) %>% 
     select(poll_correct, market_correct)
+
+logistic_mod <- logistic_reg() %>%
+    set_engine("glm") %>%
+    set_mode("classification")
 
 state_list <- c(
     "Alabama",
@@ -91,7 +95,7 @@ ui <- navbarPage(
                          selected = "2020"
                      ),
                      p(
-                         "Select a state and election year to see how candidates' polling averages and market prices changed over time. Candidates are only displayed for a given state if they did not drop out before the date of the election. If an error is displayed, that means there is insufficient data for that year/state combination."
+                         "Select a state and election year to see how candidates' polling averages and market prices changed over time. Candidates are only displayed for a given state if they did not drop out before the date of the election."
                      )
                  ),
                  mainPanel(plotOutput("statePlot"))
@@ -105,7 +109,7 @@ ui <- navbarPage(
                 choices = c("Polling Average", "Prediction Market")
             ),
             p(
-                "Change the prediction type to determine which plot is displayed. Every point represents a candidate in a specific state and election year. The predictor varaible, either market price or polling average, are plotted against the final vote percent in the election for each candidate."
+                "Select a prediction type to view the data for that variable. Every point on the plot represents a candidate in a specific state and election year. The predictor variables, either final market price or final polling average, are plotted against the vote percent in the election for each candidate. The final values are taken from the day before the election."
             ),
             p(
                 "Regression lines are also generated for each plot. A linear regression model was used for the polling average data, while a third-degree polynomial regression was used for market data to create a better fit. The polling average data displays a higher R squared value than the market price data, indicating that the polling average data fits its model more closely than the prediction market."
@@ -121,8 +125,9 @@ ui <- navbarPage(
                    gt_output("prediction_table"),
                    br(),
                    p(
-                       "To determine how accurate the two variables were at predicting election results, new variables were created to represent whether or not the markets or polls predicted that a specific candidate would win. As can be seen in the table, the prediction markets were seven percentage points more accurate than the polling averages. In total, the markets correctly predicted 4 more elections than the polls."
-                   )
+                       "To determine how accurate the two variables were at predicting election results, new variables were created to represent whether or not the markets or polls predicted that a specific candidate would win. These variables were then compared to the actual election outcomes to determine a percent accuracy. As can be seen in the table, the prediction markets were seven percentage points more accurate than the polling averages. In total, the markets correctly predicted 4 more elections than the polls. "
+                   ),
+                   p("It is important to note, however, that these predictions are based on the final market prices and polling averages from the day before the election. These levels of accuracy likely change over time leading up to the election.")
                )),
         column(6,
                wellPanel(
@@ -130,7 +135,7 @@ ui <- navbarPage(
                    gt_output("error_table"),
                    br(),
                    p(
-                       "To calculate how useful the two variables were for predicting vote percent in the election, the difference between each predictor variable and the vote percent was calculated for each candidate. The polling average was significantly better at predicting the vote percent, as would be expected given the strong linear relationship shown on the Market and Poll Data page."
+                       "Although the main purpose of the project is to examine the accuracy of predictions for election outcomes, it is also interesting to consider how well each variable predicts vote percent. To calculate how useful the two variables were for predicting vote percent in the election, the error between predictor variables and vote percent was calculated for each candidate. The polling average was significantly better at predicting the vote percent, as would be expected given the strong linear relationship shown on the Market and Poll Data page."
                    )
                ))
     ),
@@ -139,25 +144,23 @@ ui <- navbarPage(
                       column(
                           10,
                           wellPanel(
-                              h3("Visualizing the Logistic Regression"),
-                              fluidRow(
-                                  column(7, plotOutput("winning_plot")),
+                              h3("Using a Logistic Regression Model"),
+                                  plotOutput("winning_plot"),
+                              br(),
                                   p(
-                                      "Logistic regression can be used to predict the chances of a candidate winning an election based on their market price. In the model visualized above, market price is the predictor variable for the election outcome variable, which is binary. The resulting model can be used to predict a candidate's chances of winning the election based on their final prediction market price before the election. Any probability greater than 0.50 is considered a predicted win."
+                                      "A logistic regression model can be used to calculate the probability of a candidate winning an election based on their prediction market price. In the model visualized here, market price is the predictor variable for the election outcome variable, which was recorded as a 1 if a candidate won and a 0 if they lost. The resulting model can be used to predict a candidate's chances of winning the election based on their final prediction market price before the election. Any probability greater than 0.50 is considered a predicted win."
+                                  ),
+                                  p(
+                                      "However, when this model is applied to the original data set, it is no more accurate at predicting election winners than the market price by itself. This makes sense considering how close the predicted winner threshold is to the 0.50 market price. Adding additional predictor variables, such as polling average, did not increase the accuracy of the model, and sometimes even decreased the accuracy."
                                   )
-                              ),
-                              br(),
-                              h3("Making More Accurate Predictions"),
-                              gt_output("winner_table_1"),
-                              br(),
-                              p(
-                                  "When a logistic regression model is created for the data, it can be used to predict election outcomes. The table above shows that this model was five percentage points more accurate at predicting election outcomes than the Prediction Markets by themselves. However, since the same elections are being used to create the model and then test it, there is a possibility the results could have been affected by overfitting. To solve this problem, the data can be randomly divided into a training group to create the model and a testing group to evaluate its accuracy. Since the data set is relatively small, it could only be split into one training group and one testing group, so many resamplings were conducted and the accuracies were averaged."
-                              ),
-                              gt_output("winner_table_2"),
-                              br(),
-                              p(
-                                  "Even with the training sets and test sets, the logistic regression model still predicted the correct election results an average of 90.16% of the time. Adding additional predictor variables such as the polling average did not have any significant increase in accuracy, and some combinations of variables resulted in a decrease in accuracy."
-                              )
+                              # ,
+                              # br(),
+                              # h3("Making More Accurate Predictions"),
+                              # gt_output("winner_table_1"),
+                              # br(),
+                              # p(
+                              #     "This model can then be applied to the original data to predict the election outcomes. The table above shows that this model was five percentage points more accurate at predicting election outcomes than the Prediction Markets by themselves, which corresponds to three more elections predicted correctly. However, since the same elections are being used to create the model and then test it, there is a possibility the results could have been affected by overfitting. To solve this problem, the data can be randomly divided into a training group to create the model and a testing group to evaluate its accuracy. Since the data set is relatively small, it could only realistically be split into one training group and one testing group, so many resamplings were conducted and the accuracies were averaged."
+                              # )
                           )
                       ))),
     tabPanel("About",
@@ -167,17 +170,19 @@ ui <- navbarPage(
                      "In recent years, prediction markets like PredictIt.org have become popular ways of betting on the outcomes of political events. These markets allow you to buy a contract related to a specific possible outcome, and if that outcome actually happens, you will get a dollar. For example, you could buy a contract for __ cents that you think ______ will win an election, and if they actually do, the website will pay you a dollar. As more people want to buy contracts for a specific outcome that they see as more likely, the higher the price gets, making the market price a valuable prediction tool."
                  ),
                  p(
-                     "Some studies have suggested that these markets, which often have hundreds of thousands of contracts sold per day, may be better predictors of political outcomes than actual polls. The purpose of this project is to examine the predictive capacity of prediction markets as compared with polls within the context of the 2016 and 2020 Democratic primary elections. Polling and market data will be evaluated as predictors of whether or not a candidate will win as well as candidates' vote percent. This information will then be used to create even more effective prediction models."
+                     "Some studies have suggested that these markets, which often have hundreds of thousands of contracts sold per day, may be better predictors of political outcomes than actual polls. The purpose of this project is to examine the predictive capacity of prediction markets as compared with polls in the context of the 2016 and 2020 Democratic primary elections."
                  ),
                  p(
-                     "Polling data will come from fivethirtyeight's estimated polling average for each candidate. Market data will come from PredictIt.org, and the daily closing market price will be used as the default value. Election results come from the New York Times and various state websites. Candidates were included in the data set only if they had sufficient market, polling, and election results data for a specific state. States were only included if there was more than one remaining candidate with enough data. As a result, only 59 primaries and caucuses from the two election years are included in the data."
-                 )
+                     "Polling data will come from FiveThirtyEight's estimated polling average for each candidate. Market data will come from PredictIt.org, and the daily closing market price will be used as the default value. Election results come from the New York Times and various state websites. Candidates were included in the data set only if they had sufficient market, polling, and election results data for a specific state. States were only included if there was more than one remaining candidate with enough data. As a result, only 59 primaries and caucuses from the two election years are included in the data."
+                 ),
+                 h3("About Me"),
+                 p("My name is Brendan Chapuis, and I am a sophomore at Harvard College studying government and data science with a minor in economics. You can reach me at chapuis@college.harvard.edu .")
              )))
 )
 
 server <- function(input, output) {
     output$statePlot <- renderPlot({
-        dem_primary %>%
+        state_data <- dem_primary %>%
             pivot_longer(
                 cols = c(poll_estimate, market_close),
                 names_to = "prediction_type",
@@ -191,7 +196,11 @@ server <- function(input, output) {
                 date < contest_date,
                 name != "Tulsi Gabbard" &
                     name != "Deval Patrick"
-            ) %>%
+            )
+        validate(
+            need(!is.na(state_data), "Error: Insufficient data for this state/year combination. Please try different values.")
+        )
+        state_data %>% 
             ggplot(aes(
                 x = date,
                 y = value,
@@ -202,7 +211,7 @@ server <- function(input, output) {
             theme_gray() +
             labs(title = "Closing Prediction Market Price and Polling Average",
                  x = "Date",
-                 y = "Polling Average (%) / Prediction Market Price ($)") +
+                 y = "Polling Average / Prediction Market Price") +
             scale_color_discrete(
                 name = "Prediction Type",
                 labels = c("Prediction Market Price",
@@ -282,8 +291,8 @@ server <- function(input, output) {
             ) %>%
             gt() %>%
             tab_header(title = "Percent Accuracy for Predicting Winner") %>%
-            cols_label(poll_accuracy = "Polling Average",
-                       market_accuracy = "Prediction Market Price")
+            cols_label(poll_accuracy = "Accuracy of Polling Average",
+                       market_accuracy = "Accuracy of Prediction Market")
     )
     
     output$error_table <- render_gt(
@@ -301,42 +310,11 @@ server <- function(input, output) {
             tab_header(title = "Average Percentage Point Error",
                        subtitle = "For Predicting Vote Percent") %>%
             cols_label(
-                average_poll_error = "Polling Average",
-                average_market_error = "Prediction Market Price"
+                average_poll_error = "Polling Error",
+                average_market_error = "Prediction Market Error"
             )
     )
     
-    output$winner_table_1 <- render_gt(
-        logistic_mod %>%
-            fit(factor(winner) ~ market_close, data = final_results) %>%
-            predict(new_data = final_results) %>%
-            bind_cols(final_results) %>%
-            mutate(model_predicted = .pred_class) %>%
-            mutate(model_correct = ifelse(model_predicted == winner, 1, 0)) %>%
-            summarize(model_accuracy = sum(model_correct) / n()) %>%
-            mutate(
-                difference = round((model_accuracy - .8475) * 100, 2),
-                model_accuracy = paste(round(model_accuracy * 100, 2), "%", sep = "")
-            ) %>%
-            gt() %>%
-            tab_header(title = "Accuracy of Logistic Model Predictions",
-                       subtitle = "Using Entire Original Data Set") %>%
-            cols_label(model_accuracy = "Percent Accuracy of Model",
-                       difference = "Percentage Point Increase in Accuracy")
-    )
-    
-    output$winner_table_2 <- render_gt(
-        winner_predictions %>%
-            summarize(
-                average_accuracy = paste(round(mean(model_accuracy) * 100, 2), "%", sep = ""),
-                std_error = round(std.error(model_accuracy) * 100, 2)
-            ) %>%
-            gt() %>%
-            tab_header(title = "Average Accuracy of Logistic Model Predictions",
-                       subtitle = "For 1000 Simulations with Resampling of Training and Test Group") %>%
-            cols_label(average_accuracy = "Average Accuracy",
-                       std_error = "Standard Error")
-    )
 }
 
 # Run the application
