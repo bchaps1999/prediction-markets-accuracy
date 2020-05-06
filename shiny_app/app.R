ff9c2cb2ca266d9993a0c14188af100012df0d02
bchaps1999 <chapuis@college.harvard.edul>
2020-05-06 19:07:59 -0400

    Final version

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -1,5 +1,6 @@
 
 
+
 library(shiny)
 library(shinythemes)
 library(gt)
@@ -89,7 +90,8 @@ state_list <- c(
     "Wyoming"
 )
 
-# Define UI for application that draws a histogram
+# Define UI for application
+
 ui <- navbarPage(
     theme = shinytheme("flatly"),
     "How Accurate Are Prediction Markets?",
@@ -109,7 +111,7 @@ ui <- navbarPage(
                          selected = "2020"
                      ),
                      p(
-                         "Select a state and election year to see how candidates' polling averages and market prices changed over time. Candidates are only displayed for a given state if they did not drop out before the date of the election."
+                         "This project uses data from 59 primary elections and caucuses in 2016 and 2020. Select a state and election year to see how candidates' polling averages and market prices changed over time. Candidates are only displayed for a given state if they did not drop out before the date of the election."
                      )
                  ),
                  mainPanel(plotOutput("statePlot"))
@@ -124,10 +126,10 @@ ui <- navbarPage(
                 choices = c("Polling Average", "Prediction Market")
             ),
             p(
-                "Select a prediction type to view the data for that variable. Every point on the plot represents a candidate in a specific state and election year. The predictor variables, either final market price or final polling average, are plotted against the vote percent in the election for each candidate. The final values are taken from the day before the election."
+                "Select a prediction type to view the final data for that variable. Every point on the plot represents a candidate in a specific state and election year. The predictor variables, either final market price or final polling average, are plotted against the vote percent in the election for each candidate. These are final values in the sense that they are taken from the day before each election."
             ),
             p(
-                "Regression lines are also generated for each plot. A linear regression model was used for the polling average data, while a third-degree polynomial regression was used for market data to create a better fit. The polling average data displays a higher R squared value than the market price data, indicating that the polling average data fits its model more closely than the prediction market."
+                "Regression lines are also generated for each plot to better understand the relationship between the two variables. A linear regression model was used for the polling average data, while a third-degree polynomial regression was used for the prediction market data. The model for the polling average data displays a higher R squared value than the model for the prediction market data, indicating that the polling average data fits its model more closely."
             )
         ),
         mainPanel(plotOutput("poll_market_plot")))
@@ -141,10 +143,10 @@ ui <- navbarPage(
                    gt_output("prediction_table"),
                    br(),
                    p(
-                       "To determine how accurate the two variables were at predicting election results, new variables were created to represent whether or not the markets or polls predicted that a specific candidate would win. These variables were then compared to the actual election outcomes to determine a percent accuracy. As can be seen in the table, the prediction markets were seven percentage points more accurate than the polling averages. In total, the markets correctly predicted 4 more elections than the polls. "
+                       "To determine how accurate the two variables were at predicting election results, new variables were created to represent whether or not the prediction markets or polls predicted that a specific candidate would win. These variables were then compared to the actual election outcomes to determine whether or not each prediction was correct and calculate an overall percent accuracy. As can be seen in the table, the prediction markets were seven percentage points more accurate than the polling averages. In total, the markets correctly predicted 4 more elections than the polls. "
                    ),
                    p(
-                       "It is important to note, however, that these predictions are based on the final market prices and polling averages from the day before the election. These levels of accuracy likely change over time leading up to the election."
+                       "It is important to note, however, that these predictions are based on the final market prices and polling averages from the day before the election. These levels of accuracy change over the weeks leading up to the election, as can be seen on the Predictions Over Time page."
                    )
                )),
         column(6,
@@ -153,7 +155,7 @@ ui <- navbarPage(
                    gt_output("error_table"),
                    br(),
                    p(
-                       "Although the main purpose of the project is to examine the accuracy of predictions for election outcomes, it is also interesting to consider how well each variable predicts vote percent. To calculate how useful the two variables were for predicting vote percent in the election, the error between predictor variables and vote percent was calculated for each candidate. The polling average was significantly better at predicting the vote percent, as would be expected given the strong linear relationship shown on the Market and Poll Data page."
+                       "Although the main purpose of the project is to examine the accuracy of predictions for election outcomes, it is also interesting to consider how well each variable predicts vote percent. To calculate how useful the two variables were for predicting vote percent in the election, the error between the predictor variables and vote percent was calculated for each candidate. The polling average was significantly better at predicting the vote percent, as would be expected given the strong linear relationship shown on the Market and Poll Data page."
                    )
                ))
     ),
@@ -167,7 +169,7 @@ ui <- navbarPage(
                        "A logistic regression model can be used to calculate the probability of a candidate winning an election based on their prediction market price. In the model visualized here, market price is the predictor variable for the election outcome variable, which was recorded as a 1 if a candidate won and a 0 if they lost. The resulting model can be used to predict a candidate's chances of winning the election based on their final prediction market price before the election. Any probability greater than 0.50 is considered a predicted win."
                    ),
                    p(
-                       "Although regression models can sometimes be used to make more accurate predictions, when this model is applied to the original data set, it is no more accurate at predicting election winners than the market price by itself. This makes sense considering how close the predicted winner threshold is to the 0.50 market price. Adding additional predictor variables, such as polling average, did not increase the accuracy of the model, and some even decreased the accuracy."
+                       "Although regression models can sometimes be used to make more accurate predictions, when this model is applied to the original data set, it is no more accurate at predicting election winners than the market price by itself. This makes sense considering both how close the model's predicted winner threshold is to the 0.50 market price and how accurate the markets are by themselves. Adding additional predictor variables, such as polling average, did not increase the accuracy of the model, and some even decreased the accuracy."
                    )
                )),
         column(7,
@@ -198,22 +200,39 @@ ui <- navbarPage(
     ),
     
     tabPanel("About",
-             mainPanel(wellPanel(
-                 h3("About This Project"),
-                 p(
-                     "In recent years, prediction markets like PredictIt.org have become popular ways of betting on the outcomes of political events. These markets allow you to buy a contract related to a specific possible outcome, and if that outcome actually happens, you will get a dollar. For example, you could buy a contract for __ cents that you think ______ will win an election, and if they actually do, the website will pay you a dollar. As more people want to buy contracts for a specific outcome that they see as more likely, the higher the price gets, making the market price a valuable prediction tool."
-                 ),
-                 p(
-                     "Some studies have suggested that these markets, which often have hundreds of thousands of contracts sold per day, may be better predictors of political outcomes than actual polls. The purpose of this project is to examine the predictive capacity of prediction markets as compared with polls in the context of the 2016 and 2020 Democratic primary elections."
-                 ),
-                 p(
-                     "Polling data will come from FiveThirtyEight's estimated polling average for each candidate. Market data will come from PredictIt.org, and the daily closing market price will be used as the default value. Election results come from the New York Times and various state websites. Candidates were included in the data set only if they had sufficient market, polling, and election results data for a specific state. States were only included if there was more than one remaining candidate with enough data. As a result, only 59 primaries and caucuses from the two election years are included in the data."
-                 ),
-                 h3("About Me"),
-                 p(
-                     "My name is Brendan Chapuis, and I am a sophomore at Harvard College studying government and data science with a minor in economics. You can reach me at chapuis@college.harvard.edu ."
+             column(
+                 9,
+                 wellPanel(
+                     h3("About This Project"),
+                     h4("Background"),
+                     p(
+                         "In recent years, prediction markets like PredictIt.org have become popular ways of betting on the outcomes of political events. These markets allow you to buy a contract related to a specific possible outcome, and if that outcome actually happens, your contract can be redeemed for a dollar. For example, you could buy a contract for __ cents that you think ______ will win an election, and if they actually do, the website will pay you a dollar. As more people want to buy contracts for a specific outcome that they see as more likely, the higher the price gets, making the market price a valuable prediction tool."
+                     ),
+                     p(
+                         "Some studies have suggested that these markets, which often have hundreds of thousands of contracts sold per day, may be better predictors of political outcomes than actual polls. The purpose of this project is to examine the predictive capacity of prediction markets as compared with polls in the context of the 2016 and 2020 Democratic primary elections."
+                     ),
+                     h4("Data Sources"),
+                     p(HTML(
+                         paste0(
+                             'The polling data used in this project is from ',
+                             a(href = 'https://github.com/fivethirtyeight/data/tree/master/polls', 'FiveThirtyEight\'s estimated polling average'),
+                             ' for each candidate. The prediction market data is from ',
+                             a(href = 'predictit.org', 'PredictIt.org'),
+                             ', and the daily closing market price will be used as the default value. Election results are from the ',
+                             a(href = 'https://www.nytimes.com/news-event/2020-election', 'New York Times'),
+                             ' and various state websites. Candidates were included in the data set only if they had sufficient market, polling, and election results data for a specific state. States were only included if there was more than one remaining candidate with enough data. As a result, only 59 primaries and caucuses from the two election years are included in the data.'
+                         )
+                     )),
+                     h3("About the Author"),
+                     p(
+                         "Brendan Chapuis is a sophomore at Harvard College studying government and data science with a minor in economics."
+                     ),
+                     h4("Contact Information"),
+                     tags$a(href = "https://github.com/bchaps1999", "GitHub"),
+                     br(),
+                     tags$a(href = "https://www.linkedin.com/in/brendan-chapuis-341703160/", "LinkedIn")
                  )
-             )))
+             ))
 )
 
 server <- function(input, output) {
@@ -248,7 +267,7 @@ server <- function(input, output) {
                 color = prediction_type
             )) +
             geom_line() +
-            facet_wrap( ~ name) +
+            facet_wrap(~ name) +
             theme_gray() +
             labs(title = "Closing Prediction Market Price and Polling Average",
                  x = "Date",
@@ -385,7 +404,7 @@ server <- function(input, output) {
                 title = "Changes in Prediction Accuracy Over Time",
                 subtitle = "Calculated As Percent of Elections Correctly Predicted",
                 x = "Days Before Election",
-                y = "Accuracy of Predictions",
+                y = "Accuracy of Predictions (%)",
                 color = "Prediction Type"
             ) +
             scale_color_discrete(labels = c("Prediction Market Price", "Polling Average")) +
